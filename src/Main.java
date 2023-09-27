4371a5e596728f05026b0b4774f007a4d6f63ccf
Caras Vitalie <carasvitalie2004@mail.ru>
2023-09-27 23:02:39 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Variables var = new Variables();
+        
+        System.out.println("Current World Population - " + var.cwp);
+        System.out.println("Cars Produced This Year - " + var.cpty);
+        System.out.println("First Letter In Alphabet - " + var.flia);
+        System.out.println("Java is Amazing - " + var.jia);
+        System.out.println(" Pi - " + var.pi);
+
+    }
+}
---
+++ src/Variables.java
@@ -0,0 +1,10 @@
+public class Variables {
+                long cwp = 7811080815L;
+                int cpty = 55235358;
+                char flia= 'A';
+                boolean jia = true;
+                float pi = 3.14159F;
+
+
+
+}
